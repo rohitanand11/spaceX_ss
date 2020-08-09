3e7b54cc307e8d8584d35edf5bb2dbd98831a44a
Rohit Anand <rohitanand3456@gmail.com>
2020-08-09 18:39:13 +0530

    Added the build from spacex_cs repo into this repo     for server side rendering.

---
+++ build/precache-manifest.a01fee2cef846298bb7cbdc35f2843a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749ab10854c75f708ce4e3a3bd597dab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d22e5d10f56999b4c0c4",
+    "url": "/static/css/main.8a54b814.chunk.css"
+  },
+  {
+    "revision": "f6097fcfbb155f4ad225",
+    "url": "/static/js/2.411b1fbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.411b1fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22e5d10f56999b4c0c4",
+    "url": "/static/js/main.d90c95be.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  }
+]);
